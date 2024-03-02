34bc5df1b3ec5549ff7a534ea52684f51aa4cff1
Aliyu <sadiqful@gmail.com>
2024-03-02 16:54:50 +0100

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,28 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily:{
+        JostB700 : ['Jost-B'],
+        JostR400 : ['Jost-R'],
+        JostM500 : ['Jost-M'],
+      }, 
+      screens : {
+        xs: '475px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        xxl: '1440px',
       },
+      zIndex :{
+        '100' : '100',
+      }
     },
   },
   plugins: [],
